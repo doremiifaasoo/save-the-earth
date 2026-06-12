4bcd6976d45a13927bf1321dbc709846ccd787cd
doremiifaasoo <cordeliak18@gmail.com>
2026-06-12 14:48:47 +0800

    first commit

---
+++ src/mascot.py
@@ -0,0 +1,32 @@
+class TurtleMascot:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.weight_grams = 100
+        self.stage = 'Egg Hatchling'
+
+    def encounter_contamination(self):
+        print(f'Oh no! Inconsistent recycling or plastic contamination made. {self.name} feels weak :-(')
+        self.health = max(0, self.health - 15)
+        if self.health == 0:
+            #redirect them to the game 
+            print(f'{self.name} is too weak to play and has checked into the Eco-Hospital.')
+        else:
+            return f'Health: {self.health}/100 | Weight: {self.weight}g'
+
+    def feed_turtle(self):
+        print(f'Yum! {self.name} loved these clean recyclables!!')
+        self.health = min(100, self.health + 5)
+        self.weight_grams += 15
+        return f'Health: {self.health}/100 | Weight: {self.weight}g'
+    
+    def get_stage(self):
+        if self.weight < 300:
+            self.stage = 'Egg Hatchling'
+            return 'Egg Hatchling'
+        elif self.weight < 1000: 
+            self.stage = 'Young Explorer'
+            return 'Young Explorer'
+        else:
+            self.stage = 'Semakau Landfill Final Boss'
+            return 'Semakau Landfill Final Boss'
---
+++ src/simulation.py
@@ -0,0 +1,35 @@
+import json
+def load_questions():
+    with open("data/questions.json", "r") as file:
+        return json.load(file)
+    
+def run_quiz(player_turtle):
+    questions = load_questions()
+    score = 0
+    questions_answered = 0
+    print("---Starting the Household Recycling Quiz---")
+
+    for q_id, q_data in questions.items():
+        print(f"{q_data['text']}")
+        for option in q_data['options']:
+            print(option)
+        while True:
+            player_answer = input("Your Answer: (A/B) or Q to Quit : ").strip().upper()
+            if player_answer in ['A', 'B', 'Q']:
+                break
+            else:
+                print("Invalid input! Please enter either A, B or Q.")
+        if player_answer == 'Q':
+            print(f"Exiting the quiz early.. saving current stats...")
+            break
+        if player_answer == q_data['correct']:
+            print("Correct!")
+            print(f"Info: {q_data['explanation']}")
+            score += 1
+            player_turtle.feed_turtle()
+        else:
+            print("Wrong Answer...")
+            print(f"The correct answer is: {q_data['correct']}")
+            print(f"Info:{q_data['explanation']}")
+            player_turtle.encounter_contamination()
+    print("Quiz finished!! Your total score: {score}/{questions_answered} points attempted.")
