222ffe488f63cf298776ec08d1aa7ae061575cb6
doremiifaasoo <cordeliak18@gmail.com>
2026-06-16 11:02:32 +0800

    edited  main menu and added gitignore lol

--- main.py
+++ main.py
@@ -1,9 +1,38 @@
 from src.mascot import TurtleMascot
 from src.simulation import run_quiz
+from src.tracker import log_bottles_recycled
+from src.funfacts import read_eco_facts
 
 def main():
-    print("Welcome to Save The Earth Game!! :-) ")
-    pet_name = input("Give your pet turtle a name: ")
+    print("Welcome to Save The Turtles!! :-) ")
+    pet_name = input("Give your pet turtle a name: ").strip()
+    if not pet_name:
+        pet_name = 'Bubbles'
     my_turtle = TurtleMascot(pet_name)
-    run_quiz(my_turtle)
-    
+    print(f"{my_turtle.name} the Turtle has hatched! Let's protect our environment")
+    while True:
+        print("---Main Menu---")
+        print(f'Mascot: {my_turtle.name} | Health: {my_turtle.health}/100 | Weight: {my_turtle.weight_grams}g')
+        print(f'Current Stage: {my_turtle.get_stage()}')
+        print('--------')
+        print('[1] Play the Educational Recycling Quiz')
+        print('[2] Log Daily Recycled Bottles *Part of the ReturnRight Scheme!*')
+        print('[3] Open the Eco Fun Facts Library')
+        print('[Q] Save and Exit Game')
+        print('--------')
+        user_selection = input("What would you like to do today? ").strip().upper()
+        if user_selection == '1':
+            run_quiz(my_turtle)
+        elif user_selection == '2':
+            log_bottles_recycled(my_turtle)
+        elif user_selection == '3':
+            read_eco_facts(my_turtle)
+        elif user_selection == 'Q':
+            print('Saving Progress.. Thank you for keeping Singapore clean and green!')
+            print(f'{my_turtle.name} will miss you! Come back soon :-(')
+            break
+        else:
+            print('Invalid input. Please enter only (1,2,3 or Q)')
+
+if __name__ == "__main__":
+    main()
--- src/simulation.py
+++ src/simulation.py
@@ -16,10 +16,12 @@ def run_quiz(player_turtle):
         while True:
             player_answer = input("Your Answer: (A/B) or Q to Quit : ").strip().upper()
             if player_answer in ['A', 'B', 'Q']:
+                questions_answered += 1
                 break
             else:
                 print("Invalid input! Please enter either A, B or Q.")
         if player_answer == 'Q':
+            questions_answered -= 1
             print(f"Exiting the quiz early.. saving current stats...")
             break
         if player_answer == q_data['correct']:
@@ -32,4 +34,4 @@ def run_quiz(player_turtle):
             print(f"The correct answer is: {q_data['correct']}")
             print(f"Info:{q_data['explanation']}")
             player_turtle.encounter_contamination()
-    print("Quiz finished!! Your total score: {score}/{questions_answered} points attempted.")
+    print(f"Quiz finished!! Your total score: {score}/{questions_answered} points attempted.")
